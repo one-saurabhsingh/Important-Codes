57039c4cfb2642fd1cc4c32fd744976483eda7bf
MARCOS <saurabh887799@gmail.com>
2024-07-23 16:17:04 +0530

    remaining part

---
+++ BitWise Tricks/bitwise.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// To find even or odd
+
+int x; cin>>x;
+
+if(x&1) cout<<"odd";
+else cout<<"even";
+
--- Recursion Codes/R print 1 sub.cpp
+++ Recursion Codes/R print 1 sub.cpp
@@ -1,39 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool f(int ind, vector<int> &ds, int s, int sum, int arr[], int n){
-    if(ind == n){
+bool f(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
+{
+    if (ind == n)
+    {
         // condition satisfied
-        if(s == sum){
-            for(auto it : ds){
-                cout<< it << " ";
+        if (s == sum)
+        {
+            for (auto it : ds)
+            {
+                cout << it << " ";
             }
-            cout<< endl;
+            cout << endl;
             return true;
         }
-        else return false;
+        else
+            return false;
     }
     // include
     ds.push_back(arr[ind]);
     s += arr[ind];
-    if(f(ind +1, ds, s, sum, arr, n) == true){
+    if (f(ind + 1, ds, s, sum, arr, n) == true)
+    {
         return true;
     }
-    s-= arr[ind];
+    s -= arr[ind];
     ds.pop_back();
     // exclude
-    if(f(ind+1, ds, s, sum, arr, n) == true){
+    if (f(ind + 1, ds, s, sum, arr, n) == true)
+    {
         return true;
     }
 
     return false;
 }
 
-int main(){
+int main()
+{
     int arr[] = {1, 2, 1};
     int n = 3;
     int sum = 2;
-    vector <int> ds;
-    f(0, ds, 0, sum , arr, n);
+    vector<int> ds;
+    f(0, ds, 0, sum, arr, n);
     return 0;
 }
